b019f897a65340feb004421914ca9850079d07e2
SpaceDog777 <dannysplace@gmail.com>
2020-10-01 22:55:22 +1300

    Final

--- GitHub/src/Git/Git.java
+++ GitHub/src/Git/Git.java
@@ -5,7 +5,8 @@ public class Git {
 	public static void main(String[] args) {
 		System.out.print("Hi, my name is Daniel"); 
 		System.out.print("My favourite colour is blue");
-		System.out.print("My favourite colour is blue");
+		System.out.print("My favourite mode of transportation is plane");
+		System.out.print("The best day of the week is Saturday");
 
 	}
 
