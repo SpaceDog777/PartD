ceeca3a471128eebd52045d438c6da5e4c4cccad
SpaceDog777 <dannysplace@gmail.com>
2020-10-01 22:48:47 +1300

    Commit

--- GitHub/src/Git/Git.java
+++ GitHub/src/Git/Git.java
@@ -3,7 +3,7 @@ package Git;
 public class Git {
 
 	public static void main(String[] args) {
-		System.out.print("Hi, my name is Daniel");
+		System.out.print("Hi, my name is Daniel"); 
 		System.out.print("My favourite colour is blue");
 
 	}
